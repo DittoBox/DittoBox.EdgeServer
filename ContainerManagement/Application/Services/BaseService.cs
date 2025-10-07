213412ae0f92611c831f3e24b09ac27376b47ab5
Diego Cancho <102440160+dcancho@users.noreply.github.com>
2025-10-07 06:48:04 -0500

    Update BaseUrl to new web service endpoint

--- ContainerManagement/Application/Services/BaseService.cs
+++ ContainerManagement/Application/Services/BaseService.cs
@@ -2,7 +2,7 @@
 {
     public abstract class BaseService
     {
-        protected string BaseUrl { get; set; } = "https://dittobox-cloud-bubuexbzeyb5fqh6.westus2-01.azurewebsites.net/api/v1/";
+        protected string BaseUrl { get; set; } = "https://dittobox-webservices.azurewebsites.net/api/v1/";
 	
 	}
 }
